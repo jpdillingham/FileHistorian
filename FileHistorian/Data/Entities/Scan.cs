33d09ae75f25d46f5a7a90795e3e213044e313b1
JP Dillingham <jpdillingham@gmail.com>
2017-06-22 18:40:00 -0500

    Tweaked the Scan class.  Added documentation, removed Files list population in constructor, and made the End timestamp nullable.

--- FileHistorian/Data/Entities/Scan.cs
+++ FileHistorian/Data/Entities/Scan.cs
@@ -5,29 +5,47 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FileHistorian.Data.Entities
 {
+    /// <summary>
+    ///     Contains the results from a scan of the filesystem.
+    /// </summary>
     public class Scan
     {
         #region Public Constructors
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Scan"/> class.
+        /// </summary>
         public Scan()
         {
             ScanID = Guid.NewGuid();
-            Files = new List<File>();
+            Start = DateTime.Now;
         }
 
         #endregion Public Constructors
 
         #region Public Properties
 
+        /// <summary>
+        ///     Gets or sets the timestamp of the end of the scan.
+        /// </summary>
         [Column(Order = 2)]
-        public DateTime End { get; set; }
+        public DateTime? End { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the list of files found during the scan.
+        /// </summary>
         public virtual List<File> Files { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the unique identifier of the scan.
+        /// </summary>
         [Key]
         [Column(Order = 0)]
         public Guid ScanID { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the timestamp of the start of the scan.
+        /// </summary>
         [Column(Order = 1)]
         public DateTime Start { get; set; }
 
