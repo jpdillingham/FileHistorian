a05a49e5e3437eec56a3f738b1e5c5797c333fdd
JP Dillingham <jpdillingham@gmail.com>
2017-06-22 18:44:17 -0500

    Added documentation to the File class.  Set the maximum length of the FullName property.

--- FileHistorian/Data/Entities/File.cs
+++ FileHistorian/Data/Entities/File.cs
@@ -4,23 +4,42 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FileHistorian.Data.Entities
 {
+    /// <summary>
+    ///     A record of a file found during a scan of the filesystem.
+    /// </summary>
     public class File
     {
         #region Public Properties
 
+        /// <summary>
+        ///     Gets or sets the timestamp at which the file was last accessed.
+        /// </summary>
         [Column(Order = 5)]
         public DateTime? AccessedOn { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the timestamp at which the file was created.
+        /// </summary>
         [Column(Order = 3)]
         public DateTime? CreatedOn { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the full name and path of the file.
+        /// </summary>
         [Key]
         [Column(Order = 1)]
+        [MaxLength(260)]
         public string FullName { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the timestamp at which the file was last modified.
+        /// </summary>
         [Column(Order = 4)]
         public DateTime? ModifiedOn { get; set; }
 
+        /// <summary>
+        ///     Gets the name of the file.
+        /// </summary>
         public string Name
         {
             get
@@ -29,6 +48,9 @@ namespace FileHistorian.Data.Entities
             }
         }
 
+        /// <summary>
+        ///     Gets the path of the file.
+        /// </summary>
         public string Path
         {
             get
@@ -37,13 +59,22 @@ namespace FileHistorian.Data.Entities
             }
         }
 
+        /// <summary>
+        ///     Gets or sets the <see cref="Scan"/> during which the file was discovered.
+        /// </summary>
         public virtual Scan Scan { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the <see cref="Scan.ScanID"/> of the <see cref="Scan"/> during which the file was discovered.
+        /// </summary>
         [Key]
         [ForeignKey("Scan")]
         [Column(Order = 0)]
         public Guid ScanID { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the size of the file.
+        /// </summary>
         [Column(Order = 2)]
         public long Size { get; set; }
 
