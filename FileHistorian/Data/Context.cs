cf559736889951fa584aff104e0d227882a66273
JP Dillingham <jpdillingham@gmail.com>
2017-06-23 18:34:22 -0500

    Added documentation for Context

--- FileHistorian/Data/Context.cs
+++ FileHistorian/Data/Context.cs
@@ -1,13 +1,24 @@
-﻿using System.Configuration;
+﻿/*
+ * Copyright (C) 2017 JP Dillingham (jp@dillingham.ws)
+ * The MIT License (MIT)
+ */
+
+using System.Configuration;
 using System.Data.Entity;
 using FileHistorian.Data.Entities;
 
 namespace FileHistorian.Data
 {
+    /// <summary>
+    ///     The database context for the application.
+    /// </summary>
     public class Context : DbContext
     {
         #region Public Constructors
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Context"/> class.
+        /// </summary>
         public Context()
         {
             Database.Connection.ConnectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
@@ -17,7 +28,14 @@ namespace FileHistorian.Data
 
         #region Public Properties
 
+        /// <summary>
+        ///     Gets or sets the list of <see cref="File"/> records for the context.
+        /// </summary>
         public DbSet<File> Files { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the list of <see cref="Scan"/> records for the context.
+        /// </summary>
         public DbSet<Scan> Scans { get; set; }
 
         #endregion Public Properties
