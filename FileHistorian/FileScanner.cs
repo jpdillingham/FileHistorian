2cd5f4ab458dbd06178655c837788efb7358433f
JP Dillingham <jpdillingham@gmail.com>
2017-06-23 19:10:48 -0500

    Stubbed FileScanner

---
+++ FileHistorian/FileScanner.cs
@@ -0,0 +1,24 @@
+﻿using FileHistorian.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHistorian
+{
+    public class FileScanner
+    {
+        #region Public Methods
+
+        public List<File> Scan(string directory)
+        {
+        }
+
+        public Task<List<File>> ScanAsync(string directory)
+        {
+        }
+
+        #endregion Public Methods
+    }
+}
