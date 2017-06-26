d35fc9252e77b112ca5aa496c6cab16afce94701
JP Dillingham <jpdillingham@gmail.com>
2017-06-25 20:37:37 -0500

    Added documentation to Configuration

--- FileHistorian/Configuration.cs
+++ FileHistorian/Configuration.cs
@@ -1,12 +1,25 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Copyright (C) 2017 JP Dillingham (jp@dillingham.ws)
+ * The MIT License (MIT)
+ */
+
+using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FileHistorian
 {
+    /// <summary>
+    ///     The <see cref="ConfigurationElement"/> used to store directories in the application configuration.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleType", Justification = "Reviewed.")]
     public class DirectoryElement : ConfigurationElement
     {
         #region Public Properties
 
+        /// <summary>
+        ///     Gets the path of the directory.
+        /// </summary>
         [ConfigurationProperty("path", IsKey = true, IsRequired = true)]
         public string Path
         {
@@ -16,10 +29,18 @@ namespace FileHistorian
         #endregion Public Properties
     }
 
+    /// <summary>
+    ///     The collection of <see cref="DirectoryElement"/> used to store directories in the application configuration.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleType", Justification = "Reviewed.")]
     public class DirectoryElementCollection : ConfigurationElementCollection, IEnumerable<DirectoryElement>
     {
         #region Public Methods
 
+        /// <summary>
+        ///     Returns an enumerator for the collection.
+        /// </summary>
+        /// <returns>An enumerator for the collection.</returns>
         public new IEnumerator<DirectoryElement> GetEnumerator()
         {
             foreach (var key in this.BaseGetAllKeys())
@@ -32,11 +53,20 @@ namespace FileHistorian
 
         #region Protected Methods
 
+        /// <summary>
+        ///     Creates a new <see cref="ConfigurationElement"/> .
+        /// </summary>
+        /// <returns>The newly created ConfigurationElement.</returns>
         protected override ConfigurationElement CreateNewElement()
         {
             return new DirectoryElement();
         }
 
+        /// <summary>
+        ///     Retrieves the key for the specified <see cref="ConfigurationElement"/> .
+        /// </summary>
+        /// <param name="element">The ConfigurationElement for which the key is to be retrieved.</param>
+        /// <returns>The retrieved key.</returns>
         protected override object GetElementKey(ConfigurationElement element)
         {
             return ((DirectoryElement)element).Path;
@@ -45,10 +75,17 @@ namespace FileHistorian
         #endregion Protected Methods
     }
 
+    /// <summary>
+    ///     The custom <see cref="ConfigurationSection"/> for the application.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleType", Justification = "Reviewed.")]
     public class FileHistorianConfigurationSection : ConfigurationSection
     {
         #region Public Properties
 
+        /// <summary>
+        ///     Gets the collection of <see cref="DirectoryElement"/> used to store directories in the application configuration.
+        /// </summary>
         [ConfigurationProperty("Directories")]
         public DirectoryElementCollection Directories
         {
