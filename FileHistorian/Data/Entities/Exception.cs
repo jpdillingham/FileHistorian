df2e179e0b37eb284e182aa26b2468d022a0b656
JP Dillingham <jpdillingham@gmail.com>
2017-06-29 22:39:23 -0500

    Added documentation to Exception

--- FileHistorian/Data/Entities/Exception.cs
+++ FileHistorian/Data/Entities/Exception.cs
@@ -1,24 +1,44 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2017 JP Dillingham (jp@dillingham.ws)
+ * The MIT License (MIT)
+ */
+
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FileHistorian.Data.Entities
 {
+    /// <summary>
+    ///     A record of an Exception encountered during a scan of the filesystem.
+    /// </summary>
     public class Exception
     {
         #region Public Properties
 
+        /// <summary>
+        ///     Gets or sets the Exception message.
+        /// </summary>
         [Column(Order = 2)]
         [MaxLength(500)]
         public string Message { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the <see cref="Scan"/> during which the Exception was encountered.
+        /// </summary>
         public virtual Scan Scan { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the <see cref="Scan.ScanID"/> of the <see cref="Scan"/> during which the file was discovered.
+        /// </summary>
         [Key]
         [ForeignKey("Scan")]
         [Column(Order = 0)]
         public Guid ScanID { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the timestamp of the time at which the Exception was encountered.
+        /// </summary>
         [Key]
         [Column(Order = 1)]
         public DateTime Timestamp { get; set; }
--- FileHistorian/Data/Migrations/201706280159573_InitialCreate.Designer.cs
+++ FileHistorian/Data/Migrations/201706280159573_InitialCreate.Designer.cs
@@ -2,7 +2,6 @@
 namespace FileHistorian.Migrations
 {
     using System.CodeDom.Compiler;
-    using System.Data.Entity.Migrations;
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
